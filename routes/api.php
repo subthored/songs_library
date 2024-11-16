947d29456770eb14ff1b6cfac2647324abe2eb0b
Eugene Zhikharev <janiqgames@gmail.com>
2024-11-16 13:09:37 +0700

    configured api routes

--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,30 @@
 <?php
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ArtistController;
 use App\Http\Controllers\AlbumController;
 use App\Http\Controllers\SongController;
-use Illuminate\Support\Facades\Route;
 
+// API Routes
 
-Route::apiResource('artists', ArtistController::class);
-Route::apiResource('albums', AlbumController::class);
-Route::apiResource('songs', SongController::class);
+// Artist Routes
+Route::get('artists', [ArtistController::class, 'index'])->name('artists.index'); // Получить всех исполнителей
+Route::get('artists/{id}', [ArtistController::class, 'show'])->name('artists.show'); // Получить конкретного исполнителя
+Route::post('artists', [ArtistController::class, 'store'])->name('artists.store'); // Добавить исполнителя
+Route::put('artists/{id}', [ArtistController::class, 'update'])->name('artists.update'); // Обновить исполнителя
+Route::delete('artists/{id}', [ArtistController::class, 'destroy'])->name('artists.destroy'); // Удалить исполнителя
 
-Route::get('/api/documentation', function() {
-    return view('swagger.index');
-});
+// Album Routes
+Route::get('albums', [AlbumController::class, 'index'])->name('albums.index'); // Получить все альбомы
+Route::get('albums/{id}', [AlbumController::class, 'show'])->name('albums.show'); // Получить конкретный альбом
+Route::post('albums', [AlbumController::class, 'store'])->name('albums.store'); // Добавить альбом
+Route::put('albums/{id}', [AlbumController::class, 'update'])->name('albums.update'); // Обновить альбом
+Route::delete('albums/{id}', [AlbumController::class, 'destroy'])->name('albums.destroy'); // Удалить альбом
+
+// Song Routes
+Route::get('songs', [SongController::class, 'index'])->name('songs.index'); // Получить все песни
+Route::get('songs/{id}', [SongController::class, 'show'])->name('songs.show'); // Получить конкретную песню
+Route::post('songs', [SongController::class, 'store'])->name('songs.store'); // Добавить песню
+Route::put('songs/{id}', [SongController::class, 'update'])->name('songs.update'); // Обновить песню
+Route::delete('songs/{id}', [SongController::class, 'destroy'])->name('songs.destroy'); // Удалить песню
